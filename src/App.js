383221d9a3eaf77e9417ae5b76c49c5cb2789396
basilsage <dj.satoda@gmail.com>
2022-12-24 10:19:17 -0800

    Adding v2 retro design

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="mydiv">
+        <div id="mydivheader">Clickss here to move</div>
+      </div>
+
+      
+    </div>
+  );
+}
+
+
+
+export default App;
+
+
